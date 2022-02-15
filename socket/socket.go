7f0cefb911a00b844b0782152e758a5e3e22f9cc
Shukla <aditya.shukla@intel.com>
2022-02-15 21:28:01 +0530

    changes

---
+++ socket/socket.go
@@ -0,0 +1,62 @@
+package socket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"terminal/cmd"
+
+	"github.com/gorilla/websocket"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
+var ocClient *corev1client.CoreV1Client
+var namespace string
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func SocketIO(client *corev1client.CoreV1Client, namespace string) {
+	fmt.Println("Go Websocket")
+	cmd.GetPods(client)
+	setupRoutes()
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello")
+}
+
+func reader(conn *websocket.Conn) {
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p))
+		err1 := conn.WriteMessage(messageType, p)
+		if err1 != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Websocket connected successfully")
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/ws", wsEndpoint)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
---
+++ utils/download.go
@@ -0,0 +1,134 @@
+package utils
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var ocUrl = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz"
+
+func downloadPkg(filepath string) error {
+	resp, err := http.Get(ocUrl)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func untar(dst string, r io.Reader) (filename string, err error) {
+
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return filename, err
+
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	i := false
+
+	for {
+
+		header, err := tr.Next()
+		if !i {
+			filename = header.Name
+			i = true
+		}
+		///log.Println(header.Name)
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return filename, nil
+
+		// return any other error
+		case err != nil:
+			return filename, err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return filename, err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return filename, err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return filename, err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
+	//return filename, err
+}
+
+func DownloadAndUncompress(name string) string {
+	log.Println("downloading pkg")
+	err := downloadPkg(name)
+	if err != nil {
+		log.Fatal("Error downloading Package: ", err)
+	}
+
+	content, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(path)
+	log.Println("unzipping path")
+	filename, err := untar(path, content)
+	if err != nil {
+		log.Fatal("Error uncompressing package: ", err)
+	}
+
+	os.Remove(name)
+
+	return filename
+
+}
